ef01c0899b972e24305be31e5df9dbd56ab6c869
Elena Manneh <elenamanneh@gmail.com>
2024-08-24 04:02:46 -0400

    Updates

--- src/img/button-icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as github } from './github.svg';
-export { default as linkedin } from './linkedin.svg';
-export { default as mail } from './mail.svg';
-export { default as resume } from './resume.svg';
